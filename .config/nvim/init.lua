61cee582b010bfdeb9f5e0c75c2fd4d54b167213
Nicholas Tietje <tietje.n@northeastern.edu>
2024-09-24 16:39:47 -0400

    complete rework

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,5 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
+
+
+require("vim-options")
+--require("vim-options")
 require("config.lazy")
---
+++ .config/nvim/lua/vim-options.lua
@@ -0,0 +1,10 @@
+
+vim.cmd("set number")
+vim.wo.relativenumber = true
+-- Use 2 spaces for tabs
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+
+
